6caa9f8384f3faefe594acfaa7564685894c82b4
martinjrz <teksojr98@gmail.com>
2021-03-20 20:39:59 +0545

    done 5

--- front/src/signin/signin.js
+++ front/src/signin/signin.js
@@ -84,12 +84,13 @@ export const Signin = () => {
         if (res.status === 200 || res.status === 201) {
           const { rendersigninOrnot } = res.data.data;
           if (rendersigninOrnot === "true") {
+            console.log(' i amd rednering ')
             const script=await scriptsetup()
             script.onload=async ()=>{
            const gapiserver=await gapisetup()
            const authinstance=gapiserver.auth2.getAuthInstance()
            authinstance.signOut()   
-         }
+            }
            setrender(true);
            document.body.appendChild(script)
           } else if (rendersigninOrnot === "false") {
